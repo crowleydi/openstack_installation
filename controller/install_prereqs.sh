887f3c3aea9f7127993b756563861ba84085ddb9
David Crowley <kanstin@gmail.com>
2020-04-11 18:06:01 -0600

    Use $IP, make script to setup skel server

--- controller/install_prereqs.sh
+++ controller/install_prereqs.sh
@@ -11,9 +11,10 @@
 #
 # Install mariadb
 #
-
-# for Train we need to use the latest mariadb server so setup the repo
-# to install from mariadb.org
+# There is a bug in MariaDB 10.1 which is the version which "ships"
+# with Ubuntu 18.04. We need at least 10.4 so here we set things
+# up so mariadb is installed from mariadb.org.
+#
 sudo apt-get install software-properties-common
 sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
 sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.netinch.com/pub/mariadb/repo/10.4/ubuntu bionic main'
@@ -22,7 +23,7 @@ sudo apt install mariadb-server python-pymysql
 
 cat <<EOF | sudo tee /etc/mysql/mariadb.conf.d/99-openstack.cnf > /dev/null
 [mysqld]
-bind-address = 10.0.0.11
+bind-address = $IP
 
 default-storage-engine = innodb
 innodb_file_per_table = on
@@ -47,7 +48,7 @@ sudo rabbitmqctl set_permissions openstack ".*" ".*" ".*"
 #
 sudo apt install memcached python-memcache
 # fix ip address of listener
-sed s/127\\.\\0.\\0\\.1/10.0.0.11/ /etc/memcached.conf > /tmp/memcached.conf
+sed s/127\\.\\0.\\0\\.1/$IP/ /etc/memcached.conf > /tmp/memcached.conf
 sudo cp /tmp/memcached.conf /etc/memcached.conf
 rm /tmp/memcached.conf
 sudo service memcached restart
@@ -61,11 +62,11 @@ ETCD_NAME="controller"
 ETCD_DATA_DIR="/var/lib/etcd"
 ETCD_INITIAL_CLUSTER_STATE="new"
 ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster-01"
-ETCD_INITIAL_CLUSTER="controller=http://10.0.0.11:2380"
-ETCD_INITIAL_ADVERTISE_PEER_URLS="http://10.0.0.11:2380"
-ETCD_ADVERTISE_CLIENT_URLS="http://10.0.0.11:2379"
-ETCD_LISTEN_PEER_URLS="http://10.0.0.0:2380"
-ETCD_LISTEN_CLIENT_URLS="http://10.0.0.11:2379"
+ETCD_INITIAL_CLUSTER="controller=http://$IP:2380"
+ETCD_INITIAL_ADVERTISE_PEER_URLS="http://$IP:2380"
+ETCD_ADVERTISE_CLIENT_URLS="http://$IP:2379"
+ETCD_LISTEN_PEER_URLS="http://$IP:2380"
+ETCD_LISTEN_CLIENT_URLS="http://$IP:2379"
 EOF
 
 sudo systemctl enable etcd
---
+++ make_skel.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#
+# David Crowley
+# ECE 530
+#
+# install script for Ubuntu 18.04 OpenStack
+# to make the skeleton image.
+#
+
+# make sure we are on train
+sudo add-apt-repository cloud-archive:train
+
+# update the repository
+sudo apt update
+sudo apt upgrade
+
+# install the base line packages
+sudo apt install chrony python3-openstackclient
+
+# add some default machines to /etc/hosts
+cat <<EOF | sudo tee -a /etc/hosts > /dev/null
+#
+# default openstack machines
+#
+10.0.0.11	controller
+10.0.0.31	compute1
+10.0.0.32	compute2
+10.0.0.41	block1
+10.0.0.42	block2
+10.0.0.51	object1
+10.0.0.52	object2
+EOF
