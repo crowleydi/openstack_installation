e1638e3f18bd83dccebeb1421fbb9e8d7fb786c6
David Crowley <kanstin@gmail.com>
2020-04-12 20:12:33 -0600

    fixed for sure

--- controller/install_swift.sh
+++ controller/install_swift.sh
@@ -17,7 +17,7 @@ fi
 . ./admin-openrc
 
 # create service credentials
-openstack user create --domain default --password $PLACEMENT_PASS swift
+openstack user create --domain default --password $SWIFT_PASS swift
 openstack role add --project service --user swift admin
 
 # create service api
@@ -36,7 +36,7 @@ openstack endpoint create --region RegionOne \
 
 
 #
-# install the placement service
+# install the swift packages
 #
 sudo apt-get -y install swift swift-proxy python-swiftclient \
   python-keystoneclient python-keystonemiddleware \
@@ -65,7 +65,7 @@ EOF
 sudo curl -o /etc/swift/proxy-server.conf-sample https://opendev.org/openstack/swift/raw/branch/master/etc/proxy-server.conf-sample
 
 # setup the proxy config
-cat <<EOF | sudo tee -a /etc/swift/proxy-server.conf > /dev/null
+cat <<EOF | sudo tee /etc/swift/proxy-server.conf > /dev/null
 [DEFAULT]
 bind_port = 8080
 user = swift
@@ -84,7 +84,6 @@ operator_roles = admin,user
 
 [filter:authtoken]
 paste.filter_factory = keystonemiddleware.auth_token:filter_factory
-
 www_authenticate_uri = http://controller:5000
 auth_url = http://controller:5000
 memcached_servers = controller:11211
@@ -99,6 +98,101 @@ delay_auth_decision = True
 [filter:cache]
 use = egg:swift#memcache
 memcache_servers = controller:11211
+
+[filter:s3api]
+use = egg:swift#s3api
+
+[filter:s3token]
+use = egg:swift#s3token
+reseller_prefix = AUTH_
+delay_auth_decision = False
+auth_uri = http://keystonehost:5000/v3
+http_timeout = 10.0
+
+[filter:healthcheck]
+use = egg:swift#healthcheck
+
+[filter:ratelimit]
+use = egg:swift#ratelimit
+
+[filter:read_only]
+use = egg:swift#read_only
+
+[filter:domain_remap]
+use = egg:swift#domain_remap
+
+[filter:catch_errors]
+use = egg:swift#catch_errors
+
+[filter:cname_lookup]
+use = egg:swift#cname_lookup
+
+[filter:staticweb]
+use = egg:swift#staticweb
+
+[filter:formpost]
+use = egg:swift#formpost
+
+[filter:name_check]
+use = egg:swift#name_check
+
+[filter:etag-quoter]
+use = egg:swift#etag_quoter
+
+[filter:list-endpoints]
+use = egg:swift#list_endpoints
+
+[filter:proxy-logging]
+use = egg:swift#proxy_logging
+
+[filter:bulk]
+use = egg:swift#bulk
+
+[filter:slo]
+use = egg:swift#slo
+
+[filter:dlo]
+use = egg:swift#dlo
+
+[filter:container-quotas]
+use = egg:swift#container_quotas
+
+[filter:account-quotas]
+use = egg:swift#account_quotas
+
+[filter:gatekeeper]
+use = egg:swift#gatekeeper
+
+[filter:container_sync]
+use = egg:swift#container_sync
+
+[filter:xprofile]
+use = egg:swift#xprofile
+
+[filter:versioned_writes]
+use = egg:swift#versioned_writes
+
+[filter:copy]
+use = egg:swift#copy
+
+[filter:keymaster]
+use = egg:swift#keymaster
+encryption_root_secret = changeme
+
+[filter:kms_keymaster]
+use = egg:swift#kms_keymaster
+
+[filter:kmip_keymaster]
+use = egg:swift#kmip_keymaster
+
+[filter:encryption]
+use = egg:swift#encryption
+
+[filter:listing_formats]
+use = egg:swift#listing_formats
+
+[filter:symlink]
+use = egg:swift#symlink
 
 EOF
 
